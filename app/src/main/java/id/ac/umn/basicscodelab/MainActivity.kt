9c6c27bda503ae69d6be4c698a6a2c31e0b7d39b
NatzNatz <125758574+QQUway@users.noreply.github.com>
2025-02-09 15:20:21 +0700

    Columns, Rows, and Buttons
    Creating Columns, Rows, Button, and Creating columns within rows

--- app/src/main/java/id/ac/umn/basicscodelab/MainActivity.kt
+++ app/src/main/java/id/ac/umn/basicscodelab/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -35,27 +38,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(color = MaterialTheme.colorScheme.background, modifier = modifier) {
-        Greeting("android")
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = modifier.padding(24.dp)){
-            Text(
-                text = "Hello"
-            )
-            Text(
-                text = name
-            )
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary,modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = modifier.weight(1f)){
+                Text(
+                    text = "Hello"
+                )
+                Text(
+                    text = name
+                )
+            }
+            ElevatedButton(
+                onClick = {}
+            ) {
+                Text("Show More")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
